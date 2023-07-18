b24eeeb5931ec3017dfd52dde442a9ede371698c
Mateusz <pelzu12345@gmail.com>
2023-07-19 00:01:55 +0200

    first commit

---
+++ src/test/java/com/example/tibiasupporter/TibiaSupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tibiasupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaSupporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
